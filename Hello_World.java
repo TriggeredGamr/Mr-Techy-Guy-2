56953d20fa0e507b4a2ccceb9be696eeee3392e9
Andrew J Zhang <andrewjiazhang@gmail.com>
2018-06-26 10:15:07 -0700

    Commited Hello World

---
+++ Hello_World.java
@@ -0,0 +1,7 @@
+public class Hello_World {
+	public static void main(String[] args){
+		//This line below is printing "Hello, World".
+		System.out.print("Where is mah newline?\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+		System.out.println("Hello, World!");
+	}
+}
