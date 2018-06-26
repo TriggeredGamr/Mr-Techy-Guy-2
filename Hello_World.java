31738e51124e47d5b8d4ff44012a678a7e062ab3
Andrew J Zhang <andrewjiazhang@gmail.com>
2018-06-26 10:22:35 -0700

    moved to a folder

--- Hello_World.java
+++
@@ -1,7 +0,0 @@
-public class Hello_World {
-	public static void main(String[] args){
-		//This line below is printing "Hello, World".
-		System.out.print("Where is mah newline?\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-		System.out.println("Hello, World!");
-	}
-}
