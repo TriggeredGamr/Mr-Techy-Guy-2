99be98fd87d518c4ecf2307e22eacf1fdfa3aa28
Andrew J Zhang <andrewjiazhang@gmail.com>
2018-06-26 22:25:04 -0700

    ASCII pictures and all the escape commands I researched.

---
+++ Lesson 1/Lab01_01.java
@@ -0,0 +1,32 @@
+public class Lab01_01 {
+	public static void main(String[] args){
+		//This line below is printing "Hello, World".
+		System.out.print("Where is mah newline?\n\n\n\n\n");
+		System.out.println("Hello, World!");
+		//I think that the \b deletes the last letter in the quotes.
+		System.out.print("The 2nd l in lol is going to be deleted here ----->lol\b\n ");
+		//I think that a carrige return removes the line.
+		System.out.println("What is a carrige return???\r ");
+		//I found out that if you don't put a space or a letter after the escape sequence then it won't work for most of them.
+		//Well form feed might have done something but I have absolutely no idea what it does so :P.
+		System.out.println("What is form feed? Well this is form feed I wonder what it does.\f ");
+		System.out.println(" WOWOWOOWWOW \'SINGLE QUOTES :DDDDD\' COOLCOOOLCOOOL");
+		//Here is the ASCII picture thingy 
+		System.out.println(" __  __        _______        _              _____               _____        _____            _ ");
+		System.out.println("|  \\/  |      |__   __|      | |            / ____|             |_   _|      / ____|          | |");
+		System.out.println("| \\  / |_ __     | | ___  ___| |__  _   _  | |  __ _   _ _   _    | |  ___  | |     ___   ___ | |");
+		System.out.println("| |\\/| | '__|    | |/ _ \\/ __| '_ \\| | | | | | |_ | | | | | | |   | | / __| | |    / _ \\ / _ \\| |");
+		System.out.println("| |  | | |       | |  __/ (__| | | | |_| | | |__| | |_| | |_| |  _| |_\\__ \\ | |___| (_) | (_) | |");
+		System.out.println("|_|  |_|_|       |_|\\___|\\___|_| |_|\\__, |  \\_____|\\__,_|\\__, | |_____|___/  \\_____\\___/ \\___/|_|");
+		System.out.println("                                     __/ |                __/ |                                  ");
+		System.out.println("                                     |___/                |___/                                   ");
+		System.out.print("  _                           \n | |                          \n | | ____ _ _ __  _ __   __ _ \n | |/ / _`  | '_ \\| '_ \\ / _` |\n |   < (_| | |_) | |_) | (_| |\n |_|\\_\\__,_| .__/| .__/ \\__,_|\n           | |   | |          \n           |_|   |_|          ");
+
+
+
+ 
+
+ 
+
+	}
+} 
