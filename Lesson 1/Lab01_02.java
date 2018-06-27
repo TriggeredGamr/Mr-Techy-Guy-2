993e31b0db278956355fbb8d29bab1cbac52ee30
Andrew J Zhang <andrewjiazhang@gmail.com>
2018-06-26 22:49:24 -0700

    Moved all of the things to new Lab01_0x.java files

---
+++ Lesson 1/Lab01_02.java
@@ -0,0 +1,14 @@
+		public class Lab01_02 {
+	public static void main(String[] args) {
+		//Here is the ASCII picture thingy 
+		System.out.println(" __  __        _______        _              _____               _____        _____            _ ");
+		System.out.println("|  \\/  |      |__   __|      | |            / ____|             |_   _|      / ____|          | |");
+		System.out.println("| \\  / |_ __     | | ___  ___| |__  _   _  | |  __ _   _ _   _    | |  ___  | |     ___   ___ | |");
+		System.out.println("| |\\/| | '__|    | |/ _ \\/ __| '_ \\| | | | | | |_ | | | | | | |   | | / __| | |    / _ \\ / _ \\| |");
+		System.out.println("| |  | | |       | |  __/ (__| | | | |_| | | |__| | |_| | |_| |  _| |_\\__ \\ | |___| (_) | (_) | |");
+		System.out.println("|_|  |_|_|       |_|\\___|\\___|_| |_|\\__, |  \\_____|\\__,_|\\__, | |_____|___/  \\_____\\___/ \\___/|_|");
+		System.out.println("                                     __/ |                __/ |                                  ");
+		System.out.println("                                     |___/                |___/                                   ");
+	}
+}
+	
---
+++ Lesson 1/Lab01_03.java
@@ -0,0 +1,7 @@
+public class Lab01_03 {
+	public static void main(String[] args) {
+	
+	//I am awesome so I did it all in one line <o/ 
+		System.out.print("  _                           \n | |                          \n | | ____ _ _ __  _ __   __ _ \n | |/ / _` | '_ \\| '_ \\ / _` |\n |   < (_| | |_) | |_) | (_| |\n |_|\\_\\__,_| .__/| .__/ \\__,_|\n           | |   | |          \n           |_|   |_|          ");
+	}
+}
