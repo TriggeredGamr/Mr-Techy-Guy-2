6ee216f6cc855b8213f4f892ee3efd259f77068c
Andrew J Zhang <andrewjiazhang@gmail.com>
2018-07-11 10:26:37 -0700

    IDK what happened stuff spasmed and Yea i fixed it

--- Lesson 2/Lab02_CalculatorChallenge.java
+++ Lesson 2/Lab02_CalculatorChallenge.java
@@ -13,20 +13,20 @@ public class Lab02_CalculatorChallenge {
 		double secondNumber = userInput.nextDouble();
 			if (operation == 1) {
 				double sum = firstNumber + secondNumber;
-				System.out.printf("Your answer is " + "%.4f" , sum);
+				System.out.printf("Your answer is " + "%.2f" , sum);
 			} else if (operation == 2) {
 				double difference = firstNumber - secondNumber;
-				System.out.printf("Your answer is " + "%.4f" , difference);
+				System.out.printf("Your answer is " + "%.2f" , difference);
 			} else if (operation == 3) {
 				double product = firstNumber * secondNumber;
-				System.out.printf("Your answer is " + "%.4f" , product);
+				System.out.printf("Your answer is " + "%.2f" , product);
 			} else if (operation == 4) {
 				double quotient = firstNumber / secondNumber;
 				System.out.printf("Your answer is " + "%.2f" , quotient);
 			}
 		}
 		else {
-		return;
+			System.out.println("You didn't enter one of the listed numbers");
 	}
 }
 }
